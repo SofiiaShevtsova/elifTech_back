22874bd7e9c4292c76b945d884f309702a9c124c
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-06-01 14:12:57 +0300

    fix order schema

--- models/orders/ordersSchema.js
+++ models/orders/ordersSchema.js
@@ -4,31 +4,38 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const orderSchema = new Schema({
-  dishName: {
-    type: String,
-    required: [true, "Set name for dish"],
-    unique: true,
-  },
-  image: {
-    type: String,
-  },
-  price: {
-    type: String,
-  },
-  quantity: {
-    type: Number,
-  },
-  shop: {
-    type: String,
+  order: [
+    {
+      dishName: {
+        type: String,
+        required: [true, "Set name for dish"],
+        unique: true,
+      },
+      image: {
+        type: String,
+      },
+      price: {
+        type: String,
+      },
+      quantity: {
+        type: Number,
+      },
+      shop: {
+        type: String,
+      },
     },
-    dataOrder: {
-      type: String,
-    },
-    owner: {
+  ],
+  totalPrice: {
+    type: String,
+  },
+  dataOrder: {
+    type: String,
+  },
+  owner: {
     type: Schema.Types.ObjectId,
     ref: "Users",
     required: true,
-    },
+  },
 });
 
 orderSchema.post("save", (error, data, next) => {
