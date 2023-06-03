0e22e10ce46f32c7e01b39ae8d35adbfa94f244a
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-06-03 17:48:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const ordersRouter = require("./routes/orders/ordersRouter");
 
 const app = express();
 
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
